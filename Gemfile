a3ee5472716c93cf43befdcb0303607da2ec87a9
Esteban Alarcon Ceballos <nocrala.nabetse@gmail.com>
2011-05-24 18:07:13 -0500

    added formtastic

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.7'
 
 gem 'nifty-generators', :group => :development
+gem 'formtastic'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/section_roles_controller.rb
+++ app/controllers/section_roles_controller.rb
@@ -1,8 +1,15 @@
 class SectionRolesController < ApplicationController
   def new
+#   @section_role = request.section_role.build
   end
 
   def edit
+   request
+  end
+
+  private
+  def request
+    @request = Request.find(params[:request_id])
   end
 
 end
