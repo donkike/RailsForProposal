494cb74921837320be8adde6d0e2e178b6f0e9d5
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-03 12:07:35 -0500

    added some more rating

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,14 +1,18 @@
 class RatesController < ApplicationController
   
   def show
-    @section_role = SectionRole.find(params[:id])    
+     @section_role = SectionRole.find(params[:id])
+  end
+  
+  def new
+     @section_role = SectionRole.find(params[:id])  
   end
   
   def create
     @section_role = SectionRole.find(params[:section_role][:id])
     @section_role.rate(params[:section_items])
     @section_role.save
-    redirect_to @section_role.user, :notice => "Rated!"
+    redirect_to rate_path(@section_role.id), :notice => "Rated!"
   end
   
 end
