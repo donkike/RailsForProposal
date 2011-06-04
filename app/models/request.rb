4f1bdb98bd398b6c92fdaaeac1c465691f2f2512
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-04 17:05:35 -0500

    added file uploading

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,7 @@
 class Request < ActiveRecord::Base
-  attr_accessible :name, :start_date, :decision_date, :company, :comment, :filename, :response_time, :status, :average
+  attr_accessible :name, :start_date, :decision_date, :company, :comment, :filename, :response_time, :status, :average, :file_upload
+  
+  attr_accessor :file_upload
 
   validates :name, :uniqueness => true
 
@@ -7,12 +9,21 @@ class Request < ActiveRecord::Base
   has_many :sections, :through => :request_sections
   has_many :section_roles, :through => :request_sections
   
+  before_save :save_file
+  
   def section(id)
     self.request_sections.find_by_section_id(id)
   end
   
   def users    
     User.where(:id => self.section_roles.inject([]){ |a, s| a << s.user_id} )
+  end
+  
+  def save_file
+    self.filename = self.file_upload.original_filename
+    directory = "public/upload/rfp"
+    path = File.join(directory, self.filename)
+    File.open(path, "wb") { |f| f.write(self.file_upload.read) }
   end
   
 end
