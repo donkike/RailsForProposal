63e8e24355297117311e645a7871cd6568ae1ba5
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-04 18:40:30 -0500

    deleted unnecessary migrations

--- db/migrate/20110604231852_add_aggregate_to_request_section.rb
+++
@@ -1,9 +0,0 @@
-class AddAggregateToRequestSection < ActiveRecord::Migration
-  def self.up
-    add_column :request_sections, :aggregate, :float
-  end
-
-  def self.down
-    remove_column :request_sections, :aggregate
-  end
-end
--- db/migrate/20110604231915_add_aggregate_to_request.rb
+++
@@ -1,9 +0,0 @@
-class AddAggregateToRequest < ActiveRecord::Migration
-  def self.up
-    add_column :requests, :aggregate, :float
-  end
-
-  def self.down
-    remove_column :requests, :aggregate
-  end
-end
